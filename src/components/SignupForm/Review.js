fd8a8f3d97345b220c9dfb343fa60370c84e64df
Dora He <kamiyajelly@gmail.com>
2021-01-18 18:36:49 -0500

    use Context hook to store all the data using states in one file

--- src/components/SignupForm/Review.js
+++ src/components/SignupForm/Review.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {useSignupForm} from './SignupFormContext'
 
-export default function SocialForm() {
+export default function Review() {
+    const {profile, social} = useSignupForm();
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const data = {...profile, ...social};
+        console.log(data);
+        // you can send data to backend by Axios
+        alert('you are submitting! congrats');
+    }
     return (
-        <form action="">
-        Review Form
+        <form onSubmit={handleSubmit}>
+            <h2>Review all your info</h2>
+            <p><strong>Name</strong>: {profile.name}</p>
+            <p><strong>Email</strong>: {profile.email}</p>
+            <p><strong>Twitter</strong>: {social.twitter}</p>
+            <p><strong>Facebook</strong>: {social.instagram}</p>
         <input type="submit" value="Submit All Info"/>
         </form>
     )
--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -4,18 +4,21 @@ import ProfileForm from './ProfileForm'
 import SocialForm from './SocialForm'
 import Review from './Review'
 import Links from './Links'
+import { SignupFormProvider } from './SignupFormContext'
 
 export default function SignupForm() {
   return (
-    <div className='signup-form'>
-      {/* Steps and links */}
-      <Links />
-      {/* forms */}
-      <Switch>
-        <Route path='/' exact component={ProfileForm} />
-        <Route path='/social' exact component={SocialForm} />
-        <Route path='/review' exact component={Review} />
-      </Switch>
-    </div>
+    <SignupFormProvider>
+      <div className='signup-form'>
+        {/* Steps and links */}
+        <Links />
+        {/* forms */}
+        <Switch>
+          <Route path='/' exact component={ProfileForm} />
+          <Route path='/social' exact component={SocialForm} />
+          <Route path='/review' exact component={Review} />
+        </Switch>
+      </div>
+    </SignupFormProvider>
   )
 }
